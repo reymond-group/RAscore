2d44e1eabe2bf417729cc773c4360b35069cb6df
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-03 13:38:51 +0100

    Add tests and run configuation

--- tests/test_load.py
+++ tests/test_load.py
@@ -2,6 +2,16 @@
 
 import unittest
 
+from RAscore import RAscore_NN, RAscore_XGB
+
+
 class TestLoad(unittest.TestCase):
     """Test the default models can be loaded."""
-    
+
+    def test_nn(self):
+        """Test loading the tensorflow/keras models."""
+        nn_scorer = RAscore_NN.RAScorerNN()
+
+    def test_xgb(self):
+        """Test loading the XGB models."""
+        xgb_scorer = RAscore_XGB.RAScorerXGB()
